57cabcca7c374283b5b751968149a0b991de17f9
E-C-Shackelford <98488905+E-C-Shackelford@users.noreply.github.com>
2022-04-25 12:10:39 -0700

    Create my-rock-paper-scissors-game,py file and start defining player class and subclasses

---
+++ my-rock-paper-scissors-game.py
@@ -0,0 +1,23 @@
+import random
+
+"""This program plays a game of Rock, Paper, Scissors between two Players,
+and reports both Player's scores each round."""
+
+moves = ['rock', 'paper', 'scissors']
+
+"""The Player class is the parent class for all of the Players
+in this game"""
+
+class Player:
+    def move(self):
+        return 'rock'
+
+    def learn(self, my_move, their_move):
+        self.my_move = my_move
+        self.their_move = their_move
+        self.my_next_move = their_move
+
+
+class RandomPlayer(Player):
+    def move(self):
+        return random.choice(moves)
